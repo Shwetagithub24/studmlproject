4322ac3ff9ea73c12cdb2329f17deade867676e3
Shwetagithub24 <shwetazarekar6@gmail.com>
2024-10-27 20:32:35 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 from src.studmlproject.logger import logging
+from src.studmlproject.exception import CustomException
+from src.studmlproject.components.data_ingestion import DataIngestion
+from src.studmlproject.components.data_ingestion import DataIngestionConfig
+import sys
 
 if __name__=="__main__":
     logging.info("The execution has started.")
+
+    try:
+        #data_ingestion_config = DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
